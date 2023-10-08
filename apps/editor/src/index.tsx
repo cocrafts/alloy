d0b1c7b88ee1f0d696c3a6955372597948908010
Fluctlight Kayaba <kayaba.fluctlight@gmail.com>
2023-10-08 18:27:48 +0700

    reconfigure deps, eslint

--- apps/editor/src/index.tsx
+++ apps/editor/src/index.tsx
@@ -1,27 +1,26 @@
 import type { FC } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
-import { invoke }  from '@tauri-apps/api';
+import { invoke } from '@tauri-apps/api';
 
 export const App: FC = () => {
-  const onInvoke = async () => {
-    const result = await invoke('greet', { name: 'world' });
-    console.log(result, '<-- message from Tauri');
-  };
+	const onInvoke = async (): Promise<void> => {
+		const result = await invoke('greet', { name: 'world' });
+		console.log(result, '<-- message from Tauri');
+	};
 
-  return (
-    <View style={styles.container}>
-      <Text onPress={onInvoke}>App</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text onPress={onInvoke}>App</Text>
+		</View>
+	);
 };
 
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  }
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
